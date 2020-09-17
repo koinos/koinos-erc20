81a605fb7eab4736319102d1ce9243d4465a31c8
theoreticalbts <theoreticalbts@users.noreply.github.com>
2020-09-17 16:08:13 -0400

    Add license header, update Solidity version

--- contracts/KnsTokenWork.sol
+++ contracts/KnsTokenWork.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 /**************************************************************************************
  *                                                                                    *
  *                             GENERATED FILE DO NOT EDIT                             *
